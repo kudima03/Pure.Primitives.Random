77ec773a26708c67851fa6dbc0a8195bbdc0a4f0
Dmitry Kurochkin <kudima03@gmail.com>
2025-06-03 15:41:15 +0300

    Implemented RandomStringCollection.

---
+++ src/Pure.Primitives.Random/String/RandomStringCollection.cs
@@ -0,0 +1,43 @@
+﻿using Pure.Primitives.Abstractions.Number;
+using Pure.Primitives.Abstractions.String;
+using System.Collections;
+
+namespace Pure.Primitives.Random.String;
+
+public sealed record RandomStringCollection : IEnumerable<IString>
+{
+    private readonly INumber<ushort> _count;
+
+    private readonly INumber<ushort> _length;
+
+    private readonly System.Random _random;
+
+    public RandomStringCollection(INumber<ushort> count, INumber<ushort> length) : this(count, length, new System.Random()) { }
+
+    public RandomStringCollection(INumber<ushort> count, INumber<ushort> length, System.Random random)
+    {
+        _count = count;
+        _random = random;
+        _length = length;
+    }
+
+    public IEnumerator<IString> GetEnumerator()
+    {
+        return Enumerable.Range(0, _count.NumberValue).Select(_ => new RandomString(_length, _random)).GetEnumerator();
+    }
+
+    public override int GetHashCode()
+    {
+        throw new NotSupportedException();
+    }
+
+    public override string ToString()
+    {
+        throw new NotSupportedException();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
