3b185eb71503bf0e12de840e635d1f6b5ec49cf8
Dmitry Kurochkin <kudima03@gmail.com>
2025-06-04 16:00:49 +0300

    Implemented tests for RandomFloat.

---
+++ src/Tests/Pure.Primitives.Random.Tests/Number/RandomFloatTests.cs
@@ -0,0 +1,55 @@
+﻿using Pure.Primitives.Abstractions.Number;
+using Pure.Primitives.Random.Number;
+
+namespace Pure.Primitives.Random.Tests.Number;
+
+using Random = System.Random;
+
+public sealed record RandomFloatTests
+{
+    [Fact]
+    public void ProduceNormalStandardDeviationWithSharedProvider()
+    {
+        Random random = new Random();
+
+        IEnumerable<double> values = Enumerable.Range(0, 10000)
+            .Select(_ => new RandomFloat(random))
+            .Cast<INumber<float>>()
+            .Select(x => Convert.ToDouble(x.NumberValue))
+            .ToArray();
+
+        double mean = values.Average();
+        double variance = values.Select(v => Math.Pow(v - mean, 2)).Average();
+        double stdDev = Math.Sqrt(variance);
+
+        Assert.InRange(stdDev, -1, 1);
+    }
+
+    [Fact]
+    public void ProduceNormalStandardDeviation()
+    {
+        IEnumerable<int> values = Enumerable.Range(0, 10000)
+            .Select(_ => new RandomFloat())
+            .Cast<INumber<float>>()
+            .Select(x => (int)x.NumberValue)
+            .ToArray();
+
+        double mean = values.Average();
+        double variance = values.Select(v => Math.Pow(v - mean, 2)).Average();
+        double stdDev = Math.Sqrt(variance);
+
+        Assert.InRange(stdDev, -1, 1);
+    }
+
+    [Fact]
+    public void ThrowsExceptionOnGetHashCode()
+    {
+        Assert.Throws<NotSupportedException>(() => new RandomFloat().GetHashCode());
+    }
+
+    [Fact]
+    public void ThrowsExceptionOnToString()
+    {
+        Assert.Throws<NotSupportedException>(() => new RandomFloat().ToString());
+    }
+}
