6767585ed3cba004367d7f955ef5e012718970b3
Dmitry Kurochkin <kudima03@gmail.com>
2025-06-04 17:11:18 +0300

    Implemented RandomDecimal.

---
+++ src/Pure.Primitives.Random/Number/RandomDecimal.cs
@@ -0,0 +1,27 @@
+﻿using Pure.Primitives.Abstractions.Number;
+
+namespace Pure.Primitives.Random.Number;
+
+public sealed record RandomDecimal : INumber<decimal>
+{
+    private readonly System.Random _random;
+
+    public RandomDecimal() : this(new System.Random()) { }
+
+    public RandomDecimal(System.Random random)
+    {
+        _random = random;
+    }
+
+    decimal INumber<decimal>.NumberValue => Convert.ToDecimal(_random.NextDouble());
+
+    public override int GetHashCode()
+    {
+        throw new NotSupportedException();
+    }
+
+    public override string ToString()
+    {
+        throw new NotSupportedException();
+    }
+}
