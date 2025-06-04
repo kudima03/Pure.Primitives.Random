00a6f9ab77cbc71e674104de5898ddffcae7d220
Dmitry Kurochkin <kudima03@gmail.com>
2025-06-04 15:56:54 +0300

    Implemented RandomFloat.

---
+++ src/Pure.Primitives.Random/Number/RandomFloat.cs
@@ -0,0 +1,27 @@
+﻿using Pure.Primitives.Abstractions.Number;
+
+namespace Pure.Primitives.Random.Number;
+
+public sealed record RandomFloat : INumber<float>
+{
+    private readonly System.Random _random;
+
+    public RandomFloat() : this(new System.Random()) { }
+
+    public RandomFloat(System.Random random)
+    {
+        _random = random;
+    }
+
+    float INumber<float>.NumberValue => _random.NextSingle();
+
+    public override int GetHashCode()
+    {
+        throw new NotSupportedException();
+    }
+
+    public override string ToString()
+    {
+        throw new NotSupportedException();
+    }
+}
