5b0f2d70e7d4a9f643701df15fe4cf26fd40c340
Dmitry Kurochkin <kudima03@gmail.com>
2025-06-04 17:07:59 +0300

    Implemented tests for RandomDoubleCollection

---
+++ src/Tests/Pure.Primitives.Random.Tests/Number/RandomDoubleCollectionTests.cs
@@ -0,0 +1,77 @@
+﻿using Pure.Primitives.Number;
+using Pure.Primitives.Random.Number;
+using System;
+using System.Collections;
+
+namespace Pure.Primitives.Random.Tests.Number;
+
+using Random = System.Random;
+
+public sealed record RandomDoubleCollectionTests
+{
+    [Fact]
+    public void ProduceExactCount()
+    {
+        const ushort count = 1000;
+        Assert.Equal(count, new RandomDoubleCollection(new UShort(count)).Count());
+    }
+
+    [Fact]
+    public void EnumeratesAsUntyped()
+    {
+        const ushort count = 1000;
+
+        IEnumerable randoms = new RandomDoubleCollection(new UShort(count));
+
+        int i = 0;
+
+        foreach (object value in randoms)
+        {
+            i++;
+        }
+
+        Assert.Equal(count, i);
+    }
+
+    [Fact]
+    public void ProduceNormalStandardDeviationWithSharedProvider()
+    {
+        Random random = new Random();
+
+        IEnumerable<double> values = new RandomDoubleCollection(new UShort(10000), random)
+            .Select(x => x.NumberValue)
+            .ToArray();
+
+        double mean = values.Average();
+        double variance = values.Select(v => Math.Pow(v - mean, 2)).Average();
+        double stdDev = Math.Sqrt(variance);
+
+        Assert.InRange(stdDev, 0, 1);
+    }
+
+    [Fact]
+    public void ProduceNormalStandardDeviation()
+    {
+        IEnumerable<double> values = new RandomDoubleCollection(new UShort(10000))
+            .Select(x => x.NumberValue)
+            .ToArray();
+
+        double mean = values.Average();
+        double variance = values.Select(v => Math.Pow(v - mean, 2)).Average();
+        double stdDev = Math.Sqrt(variance);
+
+        Assert.InRange(stdDev, 0, 1);
+    }
+
+    [Fact]
+    public void ThrowsExceptionOnGetHashCode()
+    {
+        Assert.Throws<NotSupportedException>(() => new RandomDoubleCollection(new MinUshort()).GetHashCode());
+    }
+
+    [Fact]
+    public void ThrowsExceptionOnToString()
+    {
+        Assert.Throws<NotSupportedException>(() => new RandomDoubleCollection(new MinUshort()).ToString());
+    }
+}
