a75366a7da91bf10042d002e18038fe7f14321b8
Dmitry Kurochkin <kudima03@gmail.com>
2025-06-04 16:19:12 +0300

    Implemented RandomIntCollection.

---
+++ src/Pure.Primitives.Random/Number/RandomIntCollection.cs
@@ -0,0 +1,39 @@
+﻿using Pure.Primitives.Abstractions.Number;
+using System.Collections;
+
+namespace Pure.Primitives.Random.Number;
+
+public sealed record RandomIntCollection : IEnumerable<INumber<int>>
+{
+    private readonly INumber<ushort> _count;
+
+    private readonly System.Random _random;
+
+    public RandomIntCollection(INumber<ushort> count) : this(count, new System.Random()) { }
+
+    public RandomIntCollection(INumber<ushort> count, System.Random random)
+    {
+        _count = count;
+        _random = random;
+    }
+
+    public IEnumerator<INumber<int>> GetEnumerator()
+    {
+        return Enumerable.Range(0, _count.NumberValue).Select(_ => new RandomInt(_random)).GetEnumerator();
+    }
+
+    public override int GetHashCode()
+    {
+        throw new NotSupportedException();
+    }
+
+    public override string ToString()
+    {
+        throw new NotSupportedException();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
