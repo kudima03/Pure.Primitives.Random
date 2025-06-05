99afae8ef3aa8e4fdc1bf41cf4767532a9991896
Dmitry Kurochkin <kudima03@gmail.com>
2025-06-05 20:19:06 +0300

    Implemented RandomDate

---
+++ src/Pure.Primitives.Random/Date/RandomDate.cs
@@ -0,0 +1,41 @@
+﻿using Pure.Primitives.Abstractions.Date;
+using Pure.Primitives.Abstractions.Number;
+using Pure.Primitives.Number;
+
+namespace Pure.Primitives.Random.Date;
+
+public sealed record RandomDate : IDate
+{
+    public RandomDate() : this(new System.Random()) { }
+
+    public RandomDate(System.Random random) :
+        this(DateOnly.MinValue.AddDays(random.Next((System.DateTime.MaxValue - System.DateTime.MinValue).Days))) { }
+
+    private RandomDate(DateOnly date) :
+        this(new UShort((ushort)date.Day),
+            new UShort((ushort)date.Month),
+            new UShort((ushort)date.Year)) { }
+
+    private RandomDate(INumber<ushort> day, INumber<ushort> month, INumber<ushort> year)
+    {
+        Day = day;
+        Month = month;
+        Year = year;
+    }
+
+    public INumber<ushort> Day { get; }
+
+    public INumber<ushort> Month { get; }
+
+    public INumber<ushort> Year { get; }
+
+    public override int GetHashCode()
+    {
+        throw new NotSupportedException();
+    }
+
+    public override string ToString()
+    {
+        throw new NotSupportedException();
+    }
+}
