684d6aef92c73bfcb78f2928eed8cee9b70ce486
Dmitry Kurochkin <kudima03@gmail.com>
2025-06-05 09:01:44 +0300

    Implemented tests for RandomTime.

---
+++ src/Tests/Pure.Primitives.Random.Tests/Time/RandomTimeTests.cs
@@ -0,0 +1,153 @@
+﻿using Pure.Primitives.Abstractions.Number;
+using Pure.Primitives.Abstractions.Time;
+using Pure.Primitives.Random.Number;
+using Pure.Primitives.Random.Time;
+
+namespace Pure.Primitives.Random.Tests.Time;
+
+using Random = System.Random;
+
+public sealed record RandomTimeTests
+{
+    [Fact]
+    public void ProduceCorrectTime()
+    {
+        Random random = new Random();
+
+        IEnumerable<ITime> values = Enumerable.Range(0, 10000)
+            .Select(_ => new RandomTime(random));
+
+        Assert.All(values,
+            x => new TimeOnly(x.Hour.NumberValue, 
+                x.Minute.NumberValue,
+                x.Second.NumberValue,
+                x.Millisecond.NumberValue,
+                x.Microsecond.NumberValue));
+
+        Assert.True(values.All(x=>
+        {
+            ushort nanosecond = x.Nanosecond.NumberValue;
+            return nanosecond is >= 0 and < 1000;
+        }));
+    }
+
+    [Fact]
+    public void ProduceNormalStandardDeviationOnHour()
+    {
+        Random random = new Random();
+
+        IEnumerable<int> values = Enumerable.Range(0, 10000)
+            .Select(_ => new RandomTime(random))
+            .Cast<ITime>()
+            .Select(x => (int)x.Hour.NumberValue)
+            .ToArray();
+
+        double mean = values.Average();
+        double variance = values.Select(v => Math.Pow(v - mean, 2)).Average();
+        double stdDev = Math.Sqrt(variance);
+
+        Assert.InRange(stdDev, 6.8, 7);
+    }
+
+    [Fact]
+    public void ProduceNormalStandardDeviationOnMinute()
+    {
+        Random random = new Random();
+
+        IEnumerable<int> values = Enumerable.Range(0, 10000)
+            .Select(_ => new RandomTime(random))
+            .Cast<ITime>()
+            .Select(x => (int)x.Minute.NumberValue)
+            .ToArray();
+
+        double mean = values.Average();
+        double variance = values.Select(v => Math.Pow(v - mean, 2)).Average();
+        double stdDev = Math.Sqrt(variance);
+
+        Assert.InRange(stdDev, 17, 17.5);
+    }
+
+    [Fact]
+    public void ProduceNormalStandardDeviationOnSecond()
+    {
+        Random random = new Random();
+
+        IEnumerable<int> values = Enumerable.Range(0, 10000)
+            .Select(_ => new RandomTime(random))
+            .Cast<ITime>()
+            .Select(x => (int)x.Second.NumberValue)
+            .ToArray();
+
+        double mean = values.Average();
+        double variance = values.Select(v => Math.Pow(v - mean, 2)).Average();
+        double stdDev = Math.Sqrt(variance);
+
+        Assert.InRange(stdDev, 17, 17.5);
+    }
+
+    [Fact]
+    public void ProduceNormalStandardDeviationOnMillisecond()
+    {
+        Random random = new Random();
+
+        IEnumerable<int> values = Enumerable.Range(0, 10000)
+            .Select(_ => new RandomTime(random))
+            .Cast<ITime>()
+            .Select(x => (int)x.Millisecond.NumberValue)
+            .ToArray();
+
+        double mean = values.Average();
+        double variance = values.Select(v => Math.Pow(v - mean, 2)).Average();
+        double stdDev = Math.Sqrt(variance);
+
+        Assert.InRange(stdDev, 280, 300);
+    }
+
+    [Fact]
+    public void ProduceNormalStandardDeviationOnMicrosecond()
+    {
+        Random random = new Random();
+
+        IEnumerable<int> values = Enumerable.Range(0, 10000)
+            .Select(_ => new RandomTime(random))
+            .Cast<ITime>()
+            .Select(x => (int)x.Microsecond.NumberValue)
+            .ToArray();
+
+        double mean = values.Average();
+        double variance = values.Select(v => Math.Pow(v - mean, 2)).Average();
+        double stdDev = Math.Sqrt(variance);
+
+        Assert.InRange(stdDev, 280, 300);
+    }
+
+    [Fact]
+    public void ProduceNormalStandardDeviationOnNanosecond()
+    {
+        Random random = new Random();
+
+        IEnumerable<int> values = Enumerable.Range(0, 10000)
+            .Select(_ => new RandomTime(random))
+            .Cast<ITime>()
+            .Select(x => (int)x.Nanosecond.NumberValue)
+            .ToArray();
+
+        double mean = values.Average();
+        double variance = values.Select(v => Math.Pow(v - mean, 2)).Average();
+        double stdDev = Math.Sqrt(variance);
+
+        Assert.InRange(stdDev, 280, 300);
+    }
+
+    [Fact]
+    public void ThrowsExceptionOnGetHashCode()
+    {
+        Assert.Throws<NotSupportedException>(() => new RandomTime().GetHashCode());
+    }
+
+    [Fact]
+    public void ThrowsExceptionOnToString()
+    {
+        Assert.Throws<NotSupportedException>(() => new RandomTime().ToString());
+    }
+}
