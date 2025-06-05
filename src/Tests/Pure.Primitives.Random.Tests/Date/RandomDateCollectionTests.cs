c703e2600728ea8dabb8f1165b103e43e685fe06
Dmitry Kurochkin <kudima03@gmail.com>
2025-06-05 20:35:16 +0300

    Implemented tests for RandomDateCollection.

---
+++ src/Tests/Pure.Primitives.Random.Tests/Date/RandomDateCollectionTests.cs
@@ -0,0 +1,108 @@
+﻿using Pure.Primitives.Abstractions.Date;
+using Pure.Primitives.Number;
+using Pure.Primitives.Random.Number;
+using System.Collections;
+
+namespace Pure.Primitives.Random.Tests.Date;
+
+using Random = System.Random;
+
+public sealed record RandomDateCollectionTests
+{
+    [Fact]
+    public void ProduceExactCount()
+    {
+        const ushort count = 1000;
+        Assert.Equal(count, new RandomDateCollection(new UShort(count)).Count());
+    }
+
+    [Fact]
+    public void EnumeratesAsUntyped()
+    {
+        const ushort count = 1000;
+
+        IEnumerable randoms = new RandomDateCollection(new UShort(count));
+
+        int i = 0;
+
+        foreach (object value in randoms)
+        {
+            i++;
+        }
+
+        Assert.Equal(count, i);
+    }
+
+    [Fact]
+    public void ProduceCorrectDate()
+    {
+        Random random = new Random();
+
+        IEnumerable<IDate> values = new RandomDateCollection(new UShort(10000), random);
+
+        Assert.All(values,
+            x => new DateOnly(x.Year.NumberValue,
+                x.Month.NumberValue,
+                x.Day.NumberValue));
+    }
+
+    [Fact]
+    public void ProduceNormalStandardDeviationOnDay()
+    {
+        Random random = new Random();
+
+        IEnumerable<int> values = new RandomDateCollection(new UShort(10000), random)
+            .Select(x => (int)x.Day.NumberValue)
+            .ToArray();
+
+        double mean = values.Average();
+        double variance = values.Select(v => Math.Pow(v - mean, 2)).Average();
+        double stdDev = Math.Sqrt(variance);
+
+        Assert.InRange(stdDev, 8.7, 9);
+    }
+
+    [Fact]
+    public void ProduceNormalStandardDeviationOnMonth()
+    {
+        Random random = new Random();
+
+        IEnumerable<int> values = new RandomDateCollection(new UShort(10000), random)
+            .Select(x => (int)x.Month.NumberValue)
+            .ToArray();
+
+        double mean = values.Average();
+        double variance = values.Select(v => Math.Pow(v - mean, 2)).Average();
+        double stdDev = Math.Sqrt(variance);
+
+        Assert.InRange(stdDev, 3.4, 3.6);
+    }
+
+    [Fact]
+    public void ProduceNormalStandardDeviationOnYear()
+    {
+        Random random = new Random();
+
+        IEnumerable<int> values = new RandomDateCollection(new UShort(10000), random)
+            .Select(x => (int)x.Year.NumberValue)
+            .ToArray();
+
+        double mean = values.Average();
+        double variance = values.Select(v => Math.Pow(v - mean, 2)).Average();
+        double stdDev = Math.Sqrt(variance);
+
+        Assert.InRange(stdDev, 2750, 2950);
+    }
+
+    [Fact]
+    public void ThrowsExceptionOnGetHashCode()
+    {
+        Assert.Throws<NotSupportedException>(() => new RandomDateCollection(new MinUshort()).GetHashCode());
+    }
+
+    [Fact]
+    public void ThrowsExceptionOnToString()
+    {
+        Assert.Throws<NotSupportedException>(() => new RandomDateCollection(new MinUshort()).ToString());
+    }
+}
