ca41b5c8c980dd71af2c14cf103c3b1b09123a9f
Dmitry Kurochkin <kudima03@gmail.com>
2025-08-24 20:11:47 +0300

    Added tests for RandomInt

--- src/Tests/Pure.Primitives.Random.Tests/Number/RandomIntTests.cs
+++ src/Tests/Pure.Primitives.Random.Tests/Number/RandomIntTests.cs
@@ -1,4 +1,5 @@
 ﻿using Pure.Primitives.Abstractions.Number;
+using Pure.Primitives.Number;
 using Pure.Primitives.Random.Number;
 
 namespace Pure.Primitives.Random.Tests.Number;
@@ -7,6 +8,27 @@ using Random = System.Random;
 
 public sealed record RandomIntTests
 {
+    [Fact]
+    public void RangeAffectGeneration()
+    {
+        INumber<int> max = new RandomInt(new Int(10), new MaxInt());
+        INumber<int> min = new RandomInt(new Zero<int>(), max);
+
+        IEnumerable<int> values = Enumerable.Range(0, 10000)
+            .Select(_ => new RandomInt(min, max))
+            .Cast<INumber<int>>()
+            .Select(x => x.NumberValue)
+            .ToArray();
+
+        Assert.True(values.All(x=> min.NumberValue <= x && x < max.NumberValue));
+    }
+
+    [Fact]
+    public void ThrowsExceptionOnMinValueGreaterThanMaxValue()
+    {
+        Assert.Throws<ArgumentOutOfRangeException>(() => ((INumber<int>)new RandomInt(new MaxInt(), new MinInt())).NumberValue);
+    }
+
     [Fact]
     public void ProduceNormalStandardDeviationWithSharedProvider()
     {
