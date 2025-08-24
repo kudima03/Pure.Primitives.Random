9e2bd4bbd49c34df1228a85ab869118acccdbb10
Dmitry Kurochkin <kudima03@gmail.com>
2025-08-24 21:19:21 +0300

    RandomTime initialization improved.

--- src/Pure.Primitives.Random/Time/RandomTime.cs
+++ src/Pure.Primitives.Random/Time/RandomTime.cs
@@ -1,22 +1,25 @@
 ﻿using Pure.Primitives.Abstractions.Number;
 using Pure.Primitives.Abstractions.Time;
 using Pure.Primitives.Number;
+using Pure.Primitives.Random.Number;
 
 namespace Pure.Primitives.Random.Time;
+
+using Random = System.Random;
 
 public sealed record RandomTime : ITime
 {
     public RandomTime()
-        : this(new System.Random()) { }
+        : this(Random.Shared) { }
 
-    public RandomTime(System.Random random)
+    public RandomTime(Random random)
         : this(
-            new UShort((ushort)random.Next(24)),
-            new UShort((ushort)random.Next(60)),
-            new UShort((ushort)random.Next(60)),
-            new UShort((ushort)random.Next(1000)),
-            new UShort((ushort)random.Next(1000)),
-            new UShort((ushort)random.Next(1000))
+            new RandomUShort(new MinUshort(), new UShort(24), random),
+            new RandomUShort(new MinUshort(), new UShort(60), random),
+            new RandomUShort(new MinUshort(), new UShort(60), random),
+            new RandomUShort(new MinUshort(), new UShort(1000), random),
+            new RandomUShort(new MinUshort(), new UShort(1000), random),
+            new RandomUShort(new MinUshort(), new UShort(1000), random)
         )
     { }
 
