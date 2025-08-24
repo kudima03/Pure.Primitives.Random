9dded23c655dc8785944c13ce83cada508d5aec1
Dmitry Kurochkin <kudima03@gmail.com>
2025-08-24 16:56:39 +0300

    Added range constraint to RandomInt

--- src/Pure.Primitives.Random/Number/RandomInt.cs
+++ src/Pure.Primitives.Random/Number/RandomInt.cs
@@ -1,21 +1,32 @@
 ﻿using Pure.Primitives.Abstractions.Number;
+using Pure.Primitives.Number;
 
 namespace Pure.Primitives.Random.Number;
 
+using Random = System.Random;
+
 public sealed record RandomInt : INumber<int>
 {
-    private readonly int _numberValue;
+    private readonly Lazy<int> _lazyValue;
 
-    public RandomInt() : this(new System.Random()) { }
+    public RandomInt()
+        : this(Random.Shared) { }
 
-    public RandomInt(System.Random random)
+    public RandomInt(Random random)
+        : this(new MinInt(), new MaxInt(), random) { }
+
+    public RandomInt(INumber<int> min, INumber<int> max)
+        : this(min, max, Random.Shared) { }
+
+    public RandomInt(INumber<int> min, INumber<int> max, Random random)
+        : this(new Lazy<int>(() => random.Next(min.NumberValue, max.NumberValue))) { }
+
+    private RandomInt(Lazy<int> lazyValue)
     {
-        byte[] bytes = new byte[4];
-        random.NextBytes(bytes);
-        _numberValue = BitConverter.ToInt32(bytes);
+        _lazyValue = lazyValue;
     }
 
-    int INumber<int>.NumberValue => _numberValue;
+    int INumber<int>.NumberValue => _lazyValue.Value;
 
     public override int GetHashCode()
     {
